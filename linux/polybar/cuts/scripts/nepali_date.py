907e93401a93aef24ec721e294682361cc86ec74
voidash <ashish.thapa477@gmail.com>
2022-02-03 20:28:56 +0545

    add polybar config

---
+++ linux/polybar/cuts/scripts/nepali_date.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+import subprocess
+import sys
+
+
+req = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in req.split()]
+
+if 'nepali-datetime' in installed_packages:
+    import nepali_datetime
+    print(nepali_datetime.datetime.now().strftime(' %B %d'))
+else:
+    print('install nepali_datetime python package')
